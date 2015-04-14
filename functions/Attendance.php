5a2a141fc4aa41349125871ae4f08835def2edc8
Lokesh Devnani <lokeshthegenius@gmail.com>
2015-04-14 23:51:41 +0530

    attendance upload function about to finish

--- functions/Attendance.php
+++ functions/Attendance.php
@@ -2,9 +2,42 @@
 class Attendance {
     protected $db;
 
-    public function __construct($db){
+    public function __construct(PDO $db){
         $this->db = $db;
     }
+    public function upload($classId,$subjectId,$teacherId,$time,$absentees){
+        try{
+            $q1 = $this->db->prepare("INSERT INTO lectures (classId,subjectId,teacherId,time,uploadTime)
+            VALUES(?,?,?,?,NOW()) ");
+            $q1->execute(array($classId,$subjectId,$teacherId,$time));
+            $lectureId = $this->db->lastInsertId();
+            echo $lectureId;
+            if(!$lectureId) return false;
+
+            $sql = array();
+            foreach($absentees as $absentee){
+                $sql[] = "(". $lectureId . "," . $absentee . ")";
+            }
+
+            $sqlString = "INSERT INTO absentees (lectureId,rollno) VALUES ".implode(",",$sql);
+            $q2 = $this->db->prepare($sqlString);
+            $q2->execute();
+            echo $q2->rowCount();
+            
+        }catch (Exception $e){
+            echo $e->getMessage();
+            return false;
+        }
+
+        /*
+        $sql = array();
+        foreach( $data as $row ) {
+            $sql[] = '("'.mysql_real_escape_string($row['text']).'", '.$row['category_id'].')';
+        }
+        mysql_query('INSERT INTO table (text, category) VALUES '.implode(',', $sql));
+        */
+    }
+
     public function display(){
         echo "Hello attendance";
     }
--- insert.php
+++ insert.php
@@ -3,14 +3,16 @@ require_once 'functions/database.php';
 require_once 'functions/Attendance.php';
 
 $att = new Attendance($db);
-$att->display();
 
 if(isset($_REQUEST)){
     echo "<pre>" , print_r($_REQUEST,true) , "</pre>";
     extract($_REQUEST);
-
     if(isset($classId) && isset($subjectId) && isset($teacherId) && isset($time) && isset($absentees)){
         //upload_time -> NOW()
+        $att->display();
+        $absentees = explode(",",$absentees);
+        $time = "2015-04-08 00:00:00";
+        $att->upload($classId,$subjectId,$teacherId,$time,$absentees);
 
 
     }
